201c548f4374bf793dc861ffb7bdff5d882ea8ca
Giuseppe Pedullà <peduz91@gmail.com>
2025-06-26 20:20:19 +0200

    Risposta

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Ciao Giuseppe, come stai?");
+        System.out.println("Bene!");
     }
 }
